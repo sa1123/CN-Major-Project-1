bd9e34afbd93236c33adcfe4750d491c8d15a5cd
sa1123 <sahil2398@yahoo.com>
2022-04-29 23:20:31 +0530

    sign up and sign in skeletal

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,18 @@ module.exports.profile = function(req, res){
         title: "Users"
     });
 }
+
+// Render Sign Up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title: "FriendFace || Sign Up"
+    })
+}
+
+
+// Render Sign In page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "FriendFace || Sign In"
+    })
+}
