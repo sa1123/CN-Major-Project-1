7f5146c8574d9731c65f42ca5cd6ad64b5d5210b
sa1123 <sahil2398@yahoo.com>
2022-04-30 10:51:29 +0530

    sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 module.exports.profile = function(req, res){
     return res.render('user_profile/', {
         title: "User Profile"
@@ -23,11 +25,27 @@ module.exports.signIn = function(req, res){
 
 // get sign up data
 module.exports.create = function(req, res){
-    // Later
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user during sign up'); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error in creating user during sign up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        } else {
+            return res.redirect('back');
+        }
+    });
 }
 
 
 // sign in and create session for the user
-module.exports.create = function(req, res){
+module.exports.createSession = function(req, res){
     // Later
 }
