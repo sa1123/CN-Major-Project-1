19b115f21ce21212e48d366d9de7399db45695de
sa1123 <sahil2398@yahoo.com>
2022-05-04 19:12:02 +0530

    passport check at url

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,13 +2,6 @@ const Post = require('../models/post');
 
 module.exports.home = function(req, res){
 
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Home",
-    //         posts: posts
-    //     });
-    // });
-
     // Populate the user of each post
     Post.find({}).populate('user').exec(function(err, posts){
         return res.render('home', {
--- controllers/posts_contoller.js
+++ controllers/posts_contoller.js
@@ -1,12 +1,15 @@
 const Post = require('../models/post');
 
 module.exports.create = function(req, res){
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err, post){
-        if(err){console.log('Error in creating post'); return}
 
-        return res.redirect('back');
-    });
+    if(req.isAuthenticated()){
+        Post.create({
+            content: req.body.content,
+            user: req.user._id
+        }, function(err, post){
+            if(err){console.log('Error in creating post'); return}
+    
+            return res.redirect('back');
+        });
+    }
 }
