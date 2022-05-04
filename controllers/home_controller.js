bf576b120b0f71601c16fd750ca4cbd184a18855
sa1123 <sahil2398@yahoo.com>
2022-05-04 18:56:57 +0530

    posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,22 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    return res.render('home',{
-        title: "Home"
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // Populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Home",
+            posts: posts
+        });
     });
+
 }
 
 // module.exports.actionName = function(req, res){}
