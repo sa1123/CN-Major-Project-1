43ef6f3db1d5b03fa3f1fca25035382585420ce1
sa1123 <sahil2398@yahoo.com>
2022-05-05 19:32:51 +0530

    users list

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports.home = function(req, res){
 
@@ -12,11 +13,17 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
-            title: "Home",
-            posts: posts
+
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: "Home",
+                posts: posts,
+                all_users: users
+            });
         });
-    });
+
+
+    })
 
 }
 
