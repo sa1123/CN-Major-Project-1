8b9364264dd4b2798cbf0eb735c788c13c5f4be3
sa1123 <sahil2398@yahoo.com>
2022-06-03 00:20:38 +0530

    home_posts

--- controllers/posts_contoller.js
+++ controllers/posts_contoller.js
@@ -9,12 +9,21 @@ module.exports.create = async function(req, res){
             user: req.user._id
         });
 
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post Created"
+            });
+        }
+
         req.flash('success', 'Post published');
         return res.redirect('back');
 
     }catch(err){
         req.flash('error', err);
-        return;
+        return res.redirect('back');
     } 
 }
 
