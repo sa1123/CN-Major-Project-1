542d51c3c320c0a7283222f1e1d409f20238ccdd
sa1123 <sahil2398@yahoo.com>
2022-06-03 00:31:06 +0530

    receiving data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,8 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function(data){
-                    console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-continer>ul').prepend(newPost);
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -20,6 +21,36 @@
     }
 
     // Method to create a post in DOM
+
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+                    <p>
+                        <small>
+                            <a class="delte-post-button" href="/posts/destroy/${post._id}">X</a>
+                        </small>
+                        
+                        ${post.content}
+                        <br>
+                        <small>
+                            ${post.user.name}
+                        </small>
+                        <br>
+                    </p>
+                    <div class="post-comments">
+                        
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" cols="15" rows="1" placeholder="Add a comment" required>
+                                <input type="hidden" name="post" value="${post._id}" >
+                                <input type="submit" value="add comment">
+                            </form>
+                
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${post._id}">
+                            </ul>
+                        </div>
+                    </div>
+                </li>`
+    )}
 
     createPost();
 }
