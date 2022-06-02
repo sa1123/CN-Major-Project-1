4066e2424ef78f04424b6c05edb450b92b314edd
sa1123 <sahil2398@yahoo.com>
2022-06-03 00:56:01 +0530

    dom delete

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,6 +13,7 @@
                 success: function(data){
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-continer>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -51,6 +52,24 @@
                     </div>
                 </li>`
     )}
+
+
+    // Method to delete post from DOM
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.prevenDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                }, error: function(error){
+                    console.log(error.responseText);
+                }
+            })
+        });
+    }
 
     createPost();
 }
