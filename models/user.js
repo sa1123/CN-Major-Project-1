1cbbb25b818ca7c6cdb33adb2ae2cef5d0119dc8
sa1123 <sahil2398@yahoo.com>
2022-06-05 23:50:28 +0530

    multer for file upload

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -17,6 +21,15 @@ const userSchema = new mongoose.Schema({
 }, {
     timestamps: true
 });
+
+let storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, '/tmp/my-uploads')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.filename + '-' + Date.now())
+    }
+})
 
 const User = mongoose.model('User', userSchema);
 
