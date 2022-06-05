00b12d9996b9cd9dcdabccd92502cdb0678efeb9
sa1123 <sahil2398@yahoo.com>
2022-06-06 00:11:21 +0530

    avatar

--- models/user.js
+++ models/user.js
@@ -29,7 +29,11 @@ let storage = multer.diskStorage({
     filename: function(req, file, cb) {
         cb(null, file.filename + '-' + Date.now())
     }
-})
+});
+
+//static methods
+userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model('User', userSchema);
 
